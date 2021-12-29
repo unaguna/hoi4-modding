513cc6a81c387fee550af30214ec55a382911e67
unaguna <k.izumi.ysk@gmail.com>
2021-12-29 16:08:22 +0900

    ✨Add the `clean` task

--- src/main/kotlin/jp/unaguna/hoi4modding/gradle/Hoi4ModdingPlugin.kt
+++ src/main/kotlin/jp/unaguna/hoi4modding/gradle/Hoi4ModdingPlugin.kt
@@ -3,9 +3,12 @@ package jp.unaguna.hoi4modding.gradle
 import jp.unaguna.hoi4modding.gradle.task.CompileModTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.BasePlugin
 
 class Hoi4ModdingPlugin: Plugin<Project> {
     override fun apply(project: Project) {
+        project.plugins.apply(BasePlugin::class.java)
+
         project.tasks.create(CompileModTask.NAME, CompileModTask::class.java)
     }
 }
