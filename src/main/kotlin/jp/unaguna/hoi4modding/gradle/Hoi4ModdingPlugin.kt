5216d15fa1623f1499664308112b29c559694d4a
unaguna <k.izumi.ysk@gmail.com>
2021-12-29 13:59:58 +0900

    🚧Create an empty plugin

---
+++ src/main/kotlin/jp/unaguna/hoi4modding/gradle/Hoi4ModdingPlugin.kt
@@ -0,0 +1,10 @@
+package jp.unaguna.hoi4modding.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class Hoi4ModdingPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
+}
