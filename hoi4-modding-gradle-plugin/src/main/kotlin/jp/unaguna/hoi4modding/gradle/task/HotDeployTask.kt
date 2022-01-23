7d2ad90e9f3ce1c9a08c6ae6c5f95d99d91d3900
unaguna <k.izumi.ysk@gmail.com>
2022-01-23 18:48:27 +0900

    🐛Add annotation SkipWhenEmpty
    To avoid errors even if there are no source files when executing the task.

--- hoi4-modding-gradle-plugin/src/main/kotlin/jp/unaguna/hoi4modding/gradle/task/HotDeployTask.kt
+++ hoi4-modding-gradle-plugin/src/main/kotlin/jp/unaguna/hoi4modding/gradle/task/HotDeployTask.kt
@@ -3,16 +3,21 @@ package jp.unaguna.hoi4modding.gradle.task
 import jp.unaguna.hoi4modding.gradle.Hoi4ModExtension
 import org.gradle.api.DefaultTask
 import org.gradle.api.InvalidUserDataException
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.SkipWhenEmpty
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 
 open class HotDeployTask: DefaultTask() {
+    @get:InputDirectory
+    @get:SkipWhenEmpty
+    val deploySrcDir: File = CompileModTask.distDir(project)
+
     @TaskAction
     fun exec() {
         val hoi4ModExtension = project.extensions.getByType(Hoi4ModExtension::class.java)
         val deployDest = hoi4ModExtension.moddir
             ?: throw InvalidUserDataException("Task '$name' needs the property '${Hoi4ModExtension.NAME}.moddir'")
-
-        val deploySrcDir = CompileModTask.distDir(project)
 
         deploySrcDir.copyRecursively(deployDest, overwrite = true)
     }
