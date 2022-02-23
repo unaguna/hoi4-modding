81d4970484e1401bd04e56095e0412e80b6a2213
unaguna <k.izumi.ysk@gmail.com>
2022-02-23 20:46:58 +0900

    🚧Add "OWNER" and "CONTROLLER"

--- hoi4-java-modding/src/main/kotlin/jp/unaguna/hoi4modding/struct/ConditionState.kt
+++ hoi4-java-modding/src/main/kotlin/jp/unaguna/hoi4modding/struct/ConditionState.kt
@@ -2,7 +2,10 @@ package jp.unaguna.hoi4modding.struct
 
 import jp.unaguna.hoi4modding.struct.common.Scope
 
-abstract class ConditionState: Condition<Scope.State>()
+abstract class ConditionState: Condition<Scope.State>() {
+    val owner = fieldFactory.adjustableConditionCountry("OWNER")
+    val controller = fieldFactory.adjustableConditionCountry("CONTROLLER")
+}
 
 internal class ConcreteConditionState(initialize: ConditionState.() -> Unit): ConditionState() {
     init {
