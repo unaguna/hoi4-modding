a4eec8e5da208cc0ef07b2edca8d0cdce38b8473
unaguna <k.izumi.ysk@gmail.com>
2022-02-26 16:27:19 +0900

    🚧Add "is_claimed_by"

--- hoi4-java-modding/src/main/kotlin/jp/unaguna/hoi4modding/struct/ConditionState.kt
+++ hoi4-java-modding/src/main/kotlin/jp/unaguna/hoi4modding/struct/ConditionState.kt
@@ -6,6 +6,7 @@ abstract class ConditionState: Condition<Scope.State>() {
     override val or = fieldFactory.adjustableConditionState("OR")
     val owner = fieldFactory.adjustableConditionCountry("OWNER")
     val controller = fieldFactory.adjustableConditionCountry("CONTROLLER")
+    val isClaimedBy = fieldFactory.adjustableCountry("is_claimed_by")
 }
 
 internal class ConcreteConditionState(initialize: ConditionState.() -> Unit): ConditionState() {
